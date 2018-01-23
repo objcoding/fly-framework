185f1ddc0e46e6ad044dc900b72a239a8555a80f
zzzch <zhangchenghui.dev@gmail.com>
2018-01-24 00:13:20 +0800

    load properties

---
+++ src/main/java/cn/zhangchenghui/flyframework/constants/PropsConstant.java
@@ -0,0 +1,30 @@
+package cn.zhangchenghui.flyframework.constants;
+
+/**
+ * 配置属性常量
+ * <p>
+ * Created by chenghui.zhang on 2018/1/23.
+ */
+public interface PropsConstant {
+
+    String CONFIG_FILE = "fly.properties";
+
+    String JDBC_DRIVER = "fly.framework.jdbc.driver";
+
+    String JDBC_URL = "fly.framework.jdbc.url";
+
+    String JDBC_USERNAME = "fly.framework.jdbc.username";
+
+    String JDBC_PASSWORD = "fly.framework.jdbc.password";
+
+    String JDBC_POOL_MINPOOLSIZE = "fly.framework.jdbc.pool.minpoolsize";
+
+    String JDBC_POOL_MAXPOOLSIZE = "fly.framework.jdbc.pool.maxpoolsize";
+
+    String JDBC_POOL_ACQUIREINCREMENT = "fly.framework.jdbc.pool.acquireincrement";
+
+    String JDBC_POOL_INITIALPOOLSIZE = "fly.framework.jdbc.pool.initialPoolSize";
+
+    String APP_BASEPACKAGE = "fly.framework.app.basepackage";
+
+}
---
+++ src/main/java/cn/zhangchenghui/flyframework/utils/PropsUtil.java
@@ -0,0 +1,66 @@
+package cn.zhangchenghui.flyframework.utils;
+
+import cn.zhangchenghui.flyframework.constants.PropsConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 读取配置文件工具
+ * <p>
+ * Created by chenghui.zhang on 2018/1/22.
+ */
+public final class PropsUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropsUtil.class);
+
+    private static Properties props;
+
+    static {
+        loadProps();
+        JdbcUtil.initDataSource();
+    }
+
+    /**
+     * 加载配置文件
+     */
+    synchronized private static void loadProps() {
+        logger.info("===== 开始加载配置文件 =====");
+        props = new Properties();
+        InputStream in = null;
+        try {
+            in = PropsUtil.class.getClassLoader().getResourceAsStream(PropsConstant.CONFIG_FILE);
+            props.load(in);
+        } catch (FileNotFoundException fnfe) {
+            logger.error("未找到配置文件 => {}", fnfe);
+        } catch (IOException ioe) {
+            logger.error("读取配置文件失败 => {}", ioe);
+        } finally {
+            try {
+                if (null != in) {
+                    in.close();
+                }
+            } catch (IOException ioe) {
+                logger.error("文件流关闭失败 => {}", ioe);
+            }
+        }
+        logger.info("===== 配置文件已加载完成 =====");
+        logger.info("配置文件内容 => {}", props);
+    }
+
+    /**
+     * 获取配置信息
+     */
+    public static String getProperty(String key) {
+        if (null == props) {
+            loadProps();
+        }
+        return props.getProperty(key);
+    }
+
+
+}
