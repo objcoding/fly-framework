22bcdb684703855de0573d01903f8230abdbc9a1
zzzch <zhangchenghui.dev@gmail.com>
2018-01-25 23:45:10 +0800

    class util

---
+++ src/main/java/cn/zhangchenghui/flyframework/utils/ClassUtil.java
@@ -0,0 +1,28 @@
+package cn.zhangchenghui.flyframework.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+/**
+ * 类加载器工具
+ *
+ * Created by chenghui.zhang on 2018/1/22.
+ */
+public final class ClassUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClassUtil.class);
+
+    public static ClassLoader getClassLoader() {
+        return null;
+    }
+
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        return null;
+    }
+
+    public static Set<Class<?>> getClassSet(String packageName) {
+        return null;
+    }
+}
