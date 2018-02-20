330368bbd2cb19158ec3725dcf85809c945387e5
zch <zhangchenghui.dev@gmail.com>
2018-02-20 23:23:29 +0800

    class utils

--- src/main/java/com/objcoding/flyframework/mvc/Data.java
+++ src/main/java/com/objcoding/flyframework/mvc/Data.java
@@ -1,7 +1,18 @@
 package com.objcoding.flyframework.mvc;
 
+import java.util.Map;
+
 /**
  * Created by chenghui.zhang on 2018/1/23.
  */
 public class Data {
+    private Map<String, Object> data;
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
 }
--- src/main/java/com/objcoding/flyframework/utils/ClassUtils.java
+++ src/main/java/com/objcoding/flyframework/utils/ClassUtils.java
@@ -3,21 +3,36 @@ package com.objcoding.flyframework.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * 类加载器工具
- *
+ * <p>
  * Created by chenghui.zhang on 2018/1/22.
  */
 public final class ClassUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(ClassUtils.class);
 
+    /**
+     * 获取当前线程类加载器
+     *
+     * @return
+     */
     public static ClassLoader getClassLoader() {
         return Thread.currentThread().getContextClassLoader();
     }
 
+    /**
+     * 根据类名加载类
+     *
+     * @param className     类名
+     * @param isInitialized 是否序列化
+     * @return
+     */
     public static Class<?> loadClass(String className, boolean isInitialized) {
         Class<?> clazz;
         try {
@@ -29,8 +44,36 @@ public final class ClassUtils {
         return clazz;
     }
 
+    /**
+     * 获取指定包名下的所有类
+     *
+     * @param packageName
+     * @return
+     */
     public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<>();
 
-        return null;
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+
+                    } else if (protocol.equals("jar")) {
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+
+        return classSet;
+    }
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+
     }
 }
