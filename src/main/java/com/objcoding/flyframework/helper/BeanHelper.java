0fe1b0161b5b294809dc7da6bcf27ceef928ea6c
zch <zhangchenghui.dev@gmail.com>
2018-02-24 17:30:01 +0800

    bean

--- src/main/java/com/objcoding/flyframework/helper/BeanHelper.java
+++ src/main/java/com/objcoding/flyframework/helper/BeanHelper.java
@@ -1,7 +1,36 @@
 package com.objcoding.flyframework.helper;
 
+import com.objcoding.flyframework.utils.ReflectUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
+ * Bean容器助手类
+ *
  * Created by chenghui.zhang on 2018/1/28.
  */
 public class BeanHelper {
+
+    private static Map<Class<?>, Object> BEAN_MAP = new HashMap<>();
+
+    static {
+        Set<Class<?>> classSet = ClassHelper.getBeanClassSet();
+        for (Class<?> clazz : classSet) {
+            Object obj = ReflectUtils.newInstance(clazz);
+            BEAN_MAP.put(clazz, obj);
+        }
+    }
+
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        if (!BEAN_MAP.containsKey(clazz)) {
+            throw new RuntimeException("Bean is not exits");
+        }
+        return (T) BEAN_MAP.get(clazz);
+    }
 }
